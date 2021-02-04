23b14112cc4383ce808d771a3f2db7a83d467b10
Bindu Trikha <58632629+1208bindu@users.noreply.github.com>
2021-02-05 01:44:02 +0530

    Update server.js

--- server.js
+++ server.js
@@ -18,8 +18,18 @@ app.use(express.json());
 if(process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
+const PORT = process.env.PORT || 5000;
 
 app.use('/api/v1/transaction', transactions);
+
+app.use(express.static(path.join(__dirname, "client", "build")))
+
+// ...
+// Right before your app.listen(), add this:
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+});
+
 
 // if(process.env.NODE_ENV === 'production') {
 //   app.use(express.static('client/build'));
@@ -27,7 +37,7 @@ app.use('/api/v1/transaction', transactions);
 //   app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')));
 // }
 
-const PORT = process.env.PORT || 5000;
+
 
 app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold));
 
